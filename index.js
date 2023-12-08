0b8e7ed4038810e9a01a7b06e527f8a2168764a4
mivi-exe <144394702+mivi-exe@users.noreply.github.com>
2023-12-07 18:49:15 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,86 +1,12 @@
+let map;
 
-  //Coordinate / Speed Change
-  let long = document.getElementById("long");
-  let lat = document.getElementById("lat");
-  let speed = document.getElementById("speed");
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
 
-  async function getLong() {
-    const response_long = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
-    const response_long_json = await response_long.json();
-    console.log(response_long_json.longitude);
-    return response_long_json.longitude;
-  }
-  
-  async function getLat() {
-    const response_lat = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
-    const response_lat_json = await response_lat.json();
-    console.log(response_lat_json.latitude);
-    return response_lat_json.latitude;
-  }
-  
-  async function getSpeed() {
-    const response_speed = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
-    const response_speed_json = await response_speed.json();
-    console.log(response_speed_json.velocity);
-    return response_speed_json.velocity;
-  }
-  
-
-  async function changeInfo() {
-    const returned_long = await getLong();
-    const returned_lat = await getLat();
-    const returned_speed = await getSpeed();
-
-    console.log("After function");
-    long.innerHTML = returned_long.toFixed(4);
-    lat.innerHTML = returned_lat.toFixed(4);
-    speed.innerHTML= returned_speed.toFixed(2);
-
-    console.log(`Longitude has been changed to: ${long.innerHTML}`);
-    console.log(`Latitude has been changed to: ${lat.innerHTML}`);
-    console.log(`Speed has been changed to: ${speed.innerHTML}`);
-
-    updateMap();
-  }
-
-
-var map_long = -80.651070;
-var map_lat = 28.573469;
-
-var map = L.map('map').setView([map_lat, map_long], 6);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 25,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-var satIcon = L.icon({
-  iconUrl: 'assets/satellite.png',
-  iconSize:     [24, 24], // size of the icon 
-  iconAnchor:   [15, 23], // point of the icon which will correspond to marker's location
-  popupAnchor:  [-2, -18] // point from which the popup should open relative to the iconAnchor
-});
-
-var marker = L.marker([map_lat, map_long], {icon: satIcon}).addTo(map);
-
-
-
-async function updateMap() {
-  var new_lat = await getLat();
-  var new_long = await getLong();
-  console.log("Map coordinates");
-  console.log(new_long);
-  console.log(new_lat);
-  map.flyTo([new_lat, new_long], 6);
-  marker.setLatLng([new_lat, new_long]).update();
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  changeInfo();
-  updateMap();
-});
-
-var height_stream = document.getElementById("stream_player");
-var height_map = document.getElementById("map");
-var temp_height = height_stream.offsetHeight;
-height_map.style.height = temp_height;
+initMap();
