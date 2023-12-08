70e34e20d676f93a7475d3d88f75abf809a2fbd4
mivi-exe <mivi.exe@gmail.com>
2023-12-07 16:16:26 -0800

    added map functionality

--- index.js
+++ index.js
@@ -39,19 +39,21 @@
     console.log(`Longitude has been changed to: ${long.innerHTML}`);
     console.log(`Latitude has been changed to: ${lat.innerHTML}`);
     console.log(`Speed has been changed to: ${speed.innerHTML}`);
+
+    updateMap();
   }
 
-var map_long = -2.035;
-var map_lat = 40.57;
+var map_long = -80.651070;
+var map_lat = 28.573469;
 
-var map = L.map('map').setView([map_long, map_lat], 6);
+var map = L.map('map').setView([map_lat, map_long], 6);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([map_long, map_lat]).addTo(map);
+var marker = L.marker([map_lat, map_long]).addTo(map);
 
 
 async function updateMap() {
@@ -60,5 +62,16 @@ async function updateMap() {
   console.log("Map coordinates");
   console.log(new_long);
   console.log(new_lat);
-  map.flyTo([new_lat, new_long], 6)
+  map.flyTo([new_lat, new_long], 6);
+  marker.setLatLng([new_lat, new_long]).update();
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  changeInfo();
+  updateMap();
+});
+
+var height_stream = document.getElementById("stream_player");
+var height_map = document.getElementById("map");
+var temp_height = height_stream.offsetHeight;
+height_map.style.height = temp_height;
